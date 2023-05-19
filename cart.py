ac02c8e47f243e83ad98d84f48aabb24d9b74137
Your Name <you@example.com>
2023-05-19 19:38:33 +0300

    added instances to the cart

--- cart.py
+++ cart.py
@@ -6,8 +6,11 @@ class Cart:
         for item in self.items:
             if item['product']==product:
                 item['quantity']+=quantity
-                return
+                return "quantity updated"
+            
         self.items.append({'product':product,'quantity':quantity})
+
+
     def remove_item(self,product):
         for item in self.items:
             if item['product']==product:
@@ -15,3 +18,14 @@ class Cart:
 
     def empty_cart(self):
         self.items=[]
+
+
+cart1 = Cart()
+item_added = cart1.add_item('Apple', 3)
+print(item_added)
+
+item_added = cart1.add_item('Banana', 2)
+print(item_added) 
+
+item_added = cart1.add_item('Apple', 2)
+print(item_added)  # quantity updated
