b57910edd019c37d66e9e77e23104fd4849de5ca
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-05-19 10:17:45 +0300

    new changes made

--- sign_up.py
+++ sign_up.py
@@ -5,9 +5,15 @@ class Customer:
         self.password = password
         self.confirm_password = confirm_password
 
-    def sign_up(self):
-        new_customer = Customer(self.name, self.password, self.confirm_password, self.email)
-        return new_customer
+    def sign_up(self,name,password,confirm_password,email):
+        # new_customer = Customer(self.name, self.password, self.confirm_password, self.email)
+        # return new_customer
+       
+        if self.name ==name and self.password == password and self.confirm_password==confirm_password and self.email==email:
+            return " You have successfully signed up"
 
-new_customer = Customer("Emily Haile", "password22237828009", "haileemily@gmail.com")
+        else:
+            return "You are require dto fill in the spaces"
+
+new_customer = Customer("Emily Haile", "password22237828009", "password22237828009" ,"haileemily@gmail.com")
 print("You have signed up successfully")
