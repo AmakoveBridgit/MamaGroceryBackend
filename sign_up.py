ac2089fd0fa065b78e936fde68269ec497411eb6
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-05-19 08:26:56 +0300

    changed the class customer  class

--- sign_up.py
+++ sign_up.py
@@ -1,12 +1,13 @@
 class Customer:
-    def __init__(self,name,password,confirm_password,email):
-        self.name=name
-        self.email=email
-        self.password=password
-        self.confirm_password=confirm_password
+    def __init__(self, name, password, confirm_password, email):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.confirm_password = confirm_password
 
     def sign_up(self):
-        new_customer =Customer(self,name,password,confirm_password,email)
-        return customer()
-    new_customer=Customer("Emily Haile", "haileemily@gmail.com", "password22237828009"):
-    print("You have logged in successfuly")
+        new_customer = Customer(self.name, self.password, self.confirm_password, self.email)
+        return new_customer
+
+new_customer = Customer("Emily Haile", "password22237828009", "haileemily@gmail.com")
+print("You have signed up successfully")
